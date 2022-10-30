911bf5b9359063119240344b22f5c8ed7b94894a
arozx <dev@aspiredevelops.tk>
2022-10-30 22:17:17 +0000

    Updates

---
+++ precache-manifest.eadc749938b91d796a8397002eb57484.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dc405f10cb795036201",
+    "url": "/sitev2/static/js/main.e5f8474b.chunk.js"
+  },
+  {
+    "revision": "e8c09e008b00834d32c9",
+    "url": "/sitev2/static/js/runtime~main.82a84531.js"
+  },
+  {
+    "revision": "d2dd52b633c5eb2c7179",
+    "url": "/sitev2/static/js/2.97540507.chunk.js"
+  },
+  {
+    "revision": "47ff2cb19378ec0a519424cc3949c02e",
+    "url": "/sitev2/index.html"
+  }
+];
---
+++ static/js/main.e5f8474b.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,t){e.exports=t(4)},function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),c=t(2);t.n(c).a.createRoot(document.getElementById("root")).render(r.a.createElement(r.a.StrictMode,null))}],[[3,1,2]]]);
+//# sourceMappingURL=main.e5f8474b.chunk.js.map
