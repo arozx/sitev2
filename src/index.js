4e55d507713f220447879dd0ced41d61096983fa
arozx <dev@aspiredevelops.tk>
2022-10-30 10:39:54 +0000

    remove node_modules

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { HashRouter as Router} from 'react-router-dom'
-import App from './App'
-import About from './About'
-import Navigation from './Navigation'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Navigation/>
-    </Router>
-    <Router>
-      <App/>
-    </Router>
-    <Router>
-      <About/>
-    </Router>
   </React.StrictMode>
 )
 
